ddbe5208449cd053ddb4ed789f5b2d7a328607a7
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-11-07 21:11:08 +0100

    main updated

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from LinkedList.LinkedList import LinkedList
 from LinkedList.LinkedListManager import LinkedListManager
 import random
 
+from com.reducers.linkedListReducer.InsertReducer import InsertReducer
+from com.reducers.linkedListReducer.PrintReducer import PrintReducer
+
 if __name__ == "__main__":
     linked_list = LinkedList()
     for i in range(20):
         new_number = random.randint(1, 999)
-        LinkedListManager.insert(linked_list=linked_list, data=new_number)
-
+        linked_list = LinkedListManager.perform_action(
+            InsertReducer, linked_list, new_number)
+            
     # print the linked list
-    LinkedListManager.print_list(linked_list)
-
-    
+    LinkedListManager.perform_action(PrintReducer, linked_list)
