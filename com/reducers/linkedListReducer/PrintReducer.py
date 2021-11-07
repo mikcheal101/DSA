ab7a03c45a093514108aa25e38a7bf950e1b90ef
hirekaan.hemen <hirekaan.hemen@konga.com>
2021-11-07 21:10:46 +0100

    PrintReducer defined

---
+++ com/reducers/linkedListReducer/PrintReducer.py
@@ -0,0 +1,16 @@
+from com.reducers.ILinkedListReducer import ILinkedListReducer
+
+
+class PrintReducer(ILinkedListReducer):
+    @staticmethod
+    def perfom_action(*args):
+        linked_list = args[0]
+        current_node = linked_list.head
+        final_string = ["[+] Linked List: "]
+
+        # tranverse the list 
+        while current_node.next_node:
+            final_string.append(" -> ({})".format(current_node.data))
+            current_node = current_node.next_node
+
+        print("".join(final_string))
